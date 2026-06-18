889a95471e759339df1d6affe514dcfcbb1b7dd5
Emanuelefelixsf <manu400w@gmail.com>
2026-06-18 07:25:11 -0300

    Clear password from database configuration

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '230420',            
+    password: '',            
     database: 'bibliotecacc',
     port: 3306,
     waitForConnections: true,
